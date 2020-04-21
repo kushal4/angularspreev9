1c60b4d085b00b1675013a46d2ba7a7abcc213af
kushal4 <sensennium.kushal4@gmail.com>
2020-04-21 18:57:28 +0530

    initial support of angularspree for angular v9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {  RouterModule } from '@angular/router';
+import {routes} from "./app.routes";
+import { AppPreloadingStrategy } from './app_preloading_strategy';
 
-
-const routes: Routes = [];
+//const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    preloadingStrategy: AppPreloadingStrategy,
+    initialNavigation: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule,BrowserTransferStateModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { reducers, metaReducers } from './app.reducers';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CheckoutHeaderComponent } from './layout/checkout-header/checkout-header.component';
+import { CheckoutFooterComponent } from './layout/checkout-footer/checkout-footer.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from "@ngrx/effects";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { TransferHttpCacheModule } from '@nguniversal/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { LayoutModule } from './layout';
+import { Ng2UiAuthModule } from 'ng2-ui-auth';
+import { myAuthConfig } from './oauth_config';
+import { CoreModule } from './core';
+import { SharedModule } from './shared';
+import { ToastrModule } from 'ngx-toastr';
+import { AppPreloadingStrategy } from './app_preloading_strategy';
+import { AddressService } from './checkout/address/services/address.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckoutHeaderComponent,
+    CheckoutFooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, 
+      { metaReducers, runtimeChecks: { strictStateImmutability: true, strictActionImmutability: true } }),
+    BrowserAnimationsModule,
+    BrowserModule.withServerTransition({ appId: 'ng-spree' }),
+    BrowserTransferStateModule,
+    TransferHttpCacheModule,
+    FormsModule,
+    LayoutModule,
+    Ng2UiAuthModule.forRoot(myAuthConfig),
+    ToastrModule.forRoot({
+      timeOut: 1500,
+      positionClass: 'toast-top-center',
+      preventDuplicates: true,
+      progressAnimation: 'increasing'
+    }),
+    CoreModule,
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    }),
+    EffectsModule.forRoot()
   ],
-  providers: [],
+  
+  providers: [AppPreloadingStrategy, AddressService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
